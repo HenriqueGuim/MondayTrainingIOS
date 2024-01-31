e9f9e9f2e94da24df3f4675b69608bc0fa154e9c
HenriqueGuim <zpatins@gmail.com>
2024-01-31 14:11:41 +0000

    Initial Commit

---
+++ MondayTraining/MondayTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MondayTrainingApp.swift
+//  MondayTraining
+//
+//  Created by Henrique Guimarães on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MondayTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
